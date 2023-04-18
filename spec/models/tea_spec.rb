c97d2dcc83a43095d533186d3026f0d4dd00f808
Leo Banos Garcia <bsleo625@gmail.com>
2023-04-18 16:35:13 -0600

    adds spec for tea model

---
+++ spec/models/tea_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Tea, type: :model do
+  describe 'relationships' do
+    it {should have_many :subscriptions}
+  end
+
+  describe 'validations' do
+    it {should validate_presence_of :title, :description, :temperature, :brew_time}
+  end
+end
