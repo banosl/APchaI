039a497ec0408d0ddb5bba8aa55059b23745f55f
Leo Banos Garcia <bsleo625@gmail.com>
2023-04-19 15:57:28 -0600

    Adds index and show to Customer Controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -3,11 +3,23 @@ class Api::V1::CustomersController < ApplicationController
     customer = Customer.new(customer_params)
 
     if customer.save
-      render json: CustomerSerializer.format_customer(customer), status: 201
+      render json: CustomerSerializer.format_new_customer(customer), status: 201
     elsif Customer.find_by(email: customer_params[:email])
       render json: {"errors": "Conflict, email already in use"}, status: 409 
     else
       render json: {"errors": customer.errors.full_messages.to_sentence}, status: 400
+    end
+  end
+
+  def index
+    customers = Customer.all
+    render json: CustomerSerializer.format_customers(customers), status: 200
+  end
+
+  def show
+    customer = Customer.find_by(id: params[:id])
+    if Customer.find_by(id: params[:id])
+      render json: CustomerSerializer.format_customers_subscriptions(customer), status: 200
     end
   end
 
