f9f19369d494a0a70a6e9c4976f2d3b1722f558b
Leo Banos Garcia <bsleo625@gmail.com>
2023-04-18 18:16:34 -0600

    Adds CustomerSerializer with #format_customer

---
+++ app/serializers/customer_serializer.rb
@@ -0,0 +1,19 @@
+class CustomerSerializer
+  def self.format_customer(customer)
+    {
+      "data": {
+        "type": "customer",
+        "id": customer[:id],
+        "attributes": {
+          "first_name": customer[:first_name],
+          "last_name": customer[:last_name],
+          "email": customer[:email],
+          "address": customer[:address],
+          "city": customer[:city],
+          "state": customer[:state],
+          "zip_code": customer[:zipcode]
+        }
+      }
+    }
+  end
+end
