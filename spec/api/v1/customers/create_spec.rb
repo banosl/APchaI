d79b9d73edbf2d2ed8d718e26b25158c56c99a1c
Leo Banos Garcia <bsleo625@gmail.com>
2023-04-18 18:07:56 -0600

    Adds test for creating a customer successfully

---
+++ spec/api/v1/customers/create_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe 'POST Customer' do
+  describe 'creating a customer' do
+    it 'succesfully creates a customer' do
+      headers = { "CONTENT_TYPE" => "application/json" }
+
+      post "/api/v1/teas", headers: headers, params: JSON.generate({first_name: "Leo",
+                                                                    last_name: "Martinez",
+                                                                    email: "leo@email.com",
+                                                                    address: "123 1st St",
+                                                                    city: "Denver",
+                                                                    state: "Colorado",
+                                                                    zipcode: 80204})
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      customer = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(customer).to have_key(:data)
+      expect(customer[:data]).to have_key(:type)
+      expect(customer[:data]).to have_key(:id)
+      expect(customer[:data]).to have_key(:attributes)
+      expect(customer[:data][:attributes]).to have_key(:first_name)
+      expect(customer[:data][:attributes]).to have_key(:last_name)
+      expect(customer[:data][:attributes]).to have_key(:email)
+      expect(customer[:data][:attributes]).to have_key(:address)
+      expect(customer[:data][:attributes]).to have_key(:city)
+      expect(customer[:data][:attributes]).to have_key(:state)
+      expect(customer[:data][:attributes]).to have_key(:zipcode)
+
+      expect(customer[:data][:attributes][:first_name]).to eq("Leo")
+      expect(customer[:data][:attributes][:last_name]).to eq("Martinez")
+      expect(customer[:data][:attributes][:email]).to eq("leo@email.com")
+      expect(customer[:data][:attributes][:address]).to eq("123 1st St")
+      expect(customer[:data][:attributes][:city]).to eq("Denver")
+      expect(customer[:data][:attributes][:state]).to eq("Colorado")
+      expect(customer[:data][:attributes][:zipcode]).to eq("80204")
+    end
+
+    it 'sends an error message when one of the customer attributes is missing'
+
+    it 'sends an error message when the email is already in use'
+  end
+end
